ade786b7297d9d8745a57992f03d5e32e35cd3c3
Dipankar Gupta <46720652+dipu528447@users.noreply.github.com>
2022-12-21 23:08:58 +0600

    all tested

--- index.js
+++ index.js
@@ -4,41 +4,28 @@ const app=express();
 require('dotenv').config()
 // const jwt=require('jsonwebtoken');
 const port=process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-app.use(express());
-const corsOptions ={
-  origin:'*',
-  credentials:true,
-  optionSuccessStatus:200,
-  }
-app.use(cors(corsOptions))
-
+app.use(cors())
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hehvwbp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
- 
-  // perform actions on the collection object
-
-  async function run(){
+async function run(){
     try {
         const users = client.db("used-books-resale-market").collection("users");
         const categories = client.db("used-books-resale-market").collection("categories");
         const products = client.db("used-books-resale-market").collection("products");
         const orders=client.db("used-books-resale-market").collection("orders");
-        // app.post('/jwt',(req,res)=>{
-        //     // console.log(req.body.email)
-        //     const currentUSer=req.body;
-        //     const token=jwt.sign(currentUSer,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
-        //     res.send({token})
-        // })
+
+        app.get('/', async (req,res)=>{
+          // const query={};
+          // const cursor=db.find(query);
+          // const service=await cursor.limit(3).toArray();
+          res.send("hello world")
+        })
 
         app.post('/addUser', async (req, res) => {
-
-            
             const newUser = req.body;
             // console.log(newUser)
             const query={
@@ -232,22 +219,11 @@ client.connect(err => {
           const result = await users.updateOne(filter, updatedDoc, options);
           res.send(result);
       });
-        app.get('/', async (req,res)=>{
-          // const query={};
-          // const cursor=db.find(query);
-          // const service=await cursor.limit(3).toArray();
-          res.send("hello world")
-        })
-      
-        
     } catch (error) {
         
     }
   }
-  run().catch(err=> console.log(err))
-});
-
-
+run().catch(err=> console.log(err))
 
 app.listen(port, ()=>{
     console.log(`server is running on ${port}`)
